80eb12d80af7a9293a0148705cb51c3d85b357a0
Steve Hampton <wolfravenous@gmail.com>
2016-08-31 20:42:34 -0400

    Add title functionality for report show view

--- spec/features/creating_reports_spec.rb
+++ spec/features/creating_reports_spec.rb
@@ -11,5 +11,12 @@ RSpec.feature "Users can create new reports" do
     click_button "Create Report"
 
     expect(page).to have_content "Report has been created."
+
+    report = Report.find_by(name: "Student")
+    expect(page.current_url).to eq report_url(report)
+
+    title = "Student - Reports - ReportMaster"
+    expect(page).to have_title title
   end
+
 end
