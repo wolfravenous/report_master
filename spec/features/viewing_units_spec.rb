6c00a4f473a73797c9bb4c38509de74fb2eeeaa5
Steve Hampton <wolfravenous@gmail.com>
2016-09-12 11:23:11 -0400

    added to view test for units to verify that link back to subject is working.

--- spec/features/viewing_units_spec.rb
+++ spec/features/viewing_units_spec.rb
@@ -23,4 +23,21 @@ RSpec.feature "Users can view units" do
 
     expect(page).to have_content "The content of first unit."
   end
+
+  scenario "for a given subject" do
+    click_link "Biology"
+    expect(page).to have_content "Unit-01"
+    expect(page).to_not have_content "Animal Disease"
+
+    click_link "Unit-01"
+    within("#unit h2") do
+      expect(page).to have_content "Unit-01"
+    end
+
+    expect(page).to have_content "The content of first unit."
+
+    click_link "Biology"
+    expect(page).to have_content "#1"
+    
+  end
 end
