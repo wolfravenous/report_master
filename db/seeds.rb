d4c7c28134c4e536ba95e5060465d4381d488699
Steve Hampton <wolfravenous@gmail.com>
2016-09-12 15:30:48 -0400

    Successfully setup seed data for Subjects and Units.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bio = Subject.create(:name => "Biology")
+bio.units.create(:title => "01-The Science of Life", :content => "This would be valid content.")
+bio.units.create(:title => "02-The Chemistry of Life", :content => "This would be valid content.")
+bio.units.create(:title => "03-The Biosphere", :content => "This would be valid content.")
+
+
+
+#
+# Subject.create(name: "Biology")
+# Subject.create(name: "Chemistry")
+#
+# Unit.create(title: "01-The Science of Life", subject_id: Subject.find_by(name: "Biology").id)
+# Unit.create(title: "03-The Biosphere", subject_id: Subject.find_by(name: "Biology").id)
+# Unit.create(title: "02-Matter and Change", subject_id: Subject.find_by(name: "Chemistry").id)
+# Unit.create(title: "04-Atomic Structure", subject_id: Subject.find_by(name: "Chemistry").id)
