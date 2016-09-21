bd495c578c4d07620bf7efa860f4363c4df77305
Steve Hampton <wolfravenous@gmail.com>
2016-09-21 15:55:36 -0400

    added ability to view future units listed on the subjects show view and then when selectuing a future unit link back to the subject.

---
+++ spec/factories/future_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :future do
+    title "Example Introduction"
+    content "This is the content that would go in an example introduction"
+  end
+end
---
+++ spec/features/viewing_futures_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.feature "Users can view futures" do
+  before do
+    biology = FactoryGirl.create(:subject, name: "Biology")
+    FactoryGirl.create(:future, subject: biology, title: "Unit7", content: "The content of a future unit for the class.")
+
+    ansci = FactoryGirl.create(:subject, name: "Animal Science")
+    FactoryGirl.create(:future, subject: ansci, title: "Unit9", content: "This is the content for what student will complete in Unit 9.")
+
+    visit "/subjects"
+  end
+
+  scenario "for a given subject" do
+    click_link "Biology"
+    expect(page).to have_content "Unit7"
+    expect(page).to_not have_content "Unit9"
+
+    click_link "Unit7"
+    within("#future h2") do
+      expect(page).to have_content "Unit7"
+    end
+
+    expect(page).to have_content "The content of a future unit for the class."
+  end
+
+  scenario "for a given subject" do
+    click_link "Animal Science"
+    expect(page).to have_content "Unit9"
+    expect(page).to_not have_content "Unit7"
+
+    click_link "Unit9"
+    within("#future h2") do
+      expect(page).to have_content "Unit9"
+    end
+
+    expect(page).to have_content "This is the content for what student will complete in Unit 9."
+
+    click_link "Animal Science"
+    expect(page).to have_content "#2"
+
+  end
+end
